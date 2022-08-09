0603c1615cd73be5d261a0a97ccb73aa74e11fa8
Erica Hashert <ericahashert@gmail.com>
2022-08-09 09:08:55 -0600

    commit tree 69ab64416b24608c819607b9628ec2959925d7ec

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someFeet){
+  let result
+  if (someFeet <= 400) {
+    result = "This one is on me!"
+  }
+
+  else if (someFeet > 400 && someFeet < 2000) {
+    result = 'That will be twenty bucks.'
+  }
+
+  else if (someFeet > 2000 && someFeet < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+
+  else if (someFeet > 2500) {
+      result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+ return someCity === "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+const tipType = 'generous'
+
+function switchOnCharmFromTip(tipType){
+
+switch (tipType) {
+  case 'generous':
+    return "Thank you so much."
+  case 'not as generous':
+    return "Thank you."
+    break;
+  default:
+    return "Bye."
+}
 }
